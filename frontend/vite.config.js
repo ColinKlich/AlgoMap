d7cfbf3c4980d764c48e044ccac837a7e3ab710c
cklich <cmklich@crimson.ua.edu>
2026-01-10 22:53:26 -0600

    added kubernetes config files. Updated backend url handling by frontend to use .env

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  esbuild: {
-    loader: "jsx",
-    include: /src\/.*\.jsx?$/,
-    exclude: [],
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    esbuild: {
+      loader: "jsx",
+      include: /src\/.*\.jsx?$/,
+      exclude: [],
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
       },
     },
-  },
+  }
 })
